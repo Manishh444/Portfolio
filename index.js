6aaf78f3209bae8324c2f144645bf0bec24e843d
manishh444 <manishbhardwaj131149@gmail.com>
2024-04-13 21:07:15 +0530

    added theme

--- index.js
+++ index.js
@@ -14,29 +14,36 @@ function PageTransitions() {
       );
       this.className += " active-btn";
     });
+  }
+
+  //sction active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      // console.log(id)
+      //resmove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      // console.log(element)
+      element.classList.add("active");
     }
-    
-    //sction active class
-    allSections.addEventListener("click", (e) => {
-      const id = e.target.dataset.id;
-      if (id) {
-        // console.log(id)
-        //resmove selected from the other btns
-        sectBtns.forEach((btn) => {
-          btn.classList.remove("active");
-        });
-        e.target.classList.add("active");
+  });
 
-        //hide other sections
-        sections.forEach((section) => {
-          section.classList.remove("active");
-        });
-
-        const element = document.getElementById(id);
-        // console.log(element)
-        element.classList.add("active");
-      }
-    });
+  //toggle theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 
 PageTransitions();
